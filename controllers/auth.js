5ccdeee211288ba9eefd8a0ac0be69039864fc3d
jaipooja <jaipooja199@gmail.com>
2025-09-12 10:32:29 +0200

    indentation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,75 +1,75 @@
-const express=require("express");
-const router=express.Router();
-const prisma=require("../prisma/db")
-const {hashPassword,jwtToken}=require("../utilities/utilities");
-const jwt=require("jsonwebtoken")
+const express = require("express");
+const router = express.Router();
+const prisma = require("../prisma/db")
+const { hashPassword, jwtToken } = require("../utilities/utilities");
+const jwt = require("jsonwebtoken")
 
 
 
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
     return res.send("Hello from home page")
     //console.log("Hello from home page")
 })
 
 
 
-router.post("/register",async(req,res)=>{
+router.post("/register", async (req, res) => {
     try {
-        const data=req.body;
-        const dataInDb=await prisma.user.findFirst({where:{username:data.username,userType:data.userType}})
-        if(dataInDb){
+        const data = req.body;
+        const dataInDb = await prisma.user.findFirst({ where: { username: data.username, userType: data.userType } })
+        if (dataInDb) {
             return res.status(409).send("username already exists!")
         }
-        else{
-            const newUser=await prisma.user.create({
-                data:{
-                    username:data.username,
-                    password:hashPassword(data.password),
-                    userType:data.userType
+        else {
+            const newUser = await prisma.user.create({
+                data: {
+                    username: data.username,
+                    password: hashPassword(data.password),
+                    userType: data.userType
                 }
             })
             // console.log(newUser)
             // return res.status(201).send("welcome to e-commerce market place")
-            newUser.password=null;
-            const token=jwtToken(newUser);
+            newUser.password = null;
+            const token = jwtToken(newUser);
             console.log(token)
-            return res.status(201).send({token:token})
+            return res.status(201).send({ token: token })
         }
-        
+
     } catch (error) {
         console.log(error)
         return res.status(500).send("something went wrong!")
 
-        
+
     }
 
 })
 
 
 
-router.post("/login",async(req , res)=>{
+router.post("/login", async (req, res) => {
     try {
-        const data=req.body;
-    const dataInDb=await prisma.user.findFirst({where:{username:data.username,userType:data.userType}})
-    if(dataInDb){
-        if(dataInDb.password==hashPassword(data.password)){
-            delete dataInDb.password;
-            const token=jwtToken(dataInDb);
-            return res.status(200).send({token:token})
+        const data = req.body;
+        const dataInDb = await prisma.user.findFirst({ where: { username: data.username, userType: data.userType } })
+        if (dataInDb) {
+            if (dataInDb.password == hashPassword(data.password)) {
+                delete dataInDb.password;
+                const token = jwtToken(dataInDb);
+                return res.status(200).send({ token: token })
+            }
+
+            else {
+                return res.status(409).json({ message: "unautrorized!!!" })
+            }
         }
 
-        else{
-            return res.status(409).json({message:"unautrorized!!!"})
+        else {
+            return res.status(404).json({ message: "not registered" })
         }
-    }
 
-    else{
-        return res.status(404).json({message:"not registered"})
-    }
-        
     } catch (error) {
         return res.status(500).send("something went wrong!!!")
-        
+
     }
 
 })
@@ -77,4 +77,4 @@ router.post("/login",async(req , res)=>{
 
 
 
-module.exports=router;
+module.exports = router;
